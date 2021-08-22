adfd0f53e319e2faac71c905d199facb94d0bcf8
pruthvigunturu <pruthvigunturu@users.github.com>
2021-08-23 03:18:38 +0530

    finished

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header className='header'>
+            <nav>
+                <div className="logo">
+                    Todoist
+                </div>
+            </nav>
+
+        </header>
+    )
+}
+
+export default Header
